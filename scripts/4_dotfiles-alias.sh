a9f531691774d045c7b79a6cd5f90811b371e3ff
Yoshiki Iwata <iwamula@i-mbp.local>
2017-10-24 17:28:54 +0900

    dotfiles-alias.shを修正

--- scripts/4_dotfiles-alias.sh
+++ scripts/4_dotfiles-alias.sh
@@ -4,7 +4,7 @@
 # ホームディレクトリに作成する
 DOTFILES_DIR=$(cd $(dirname $0)/..; pwd)
 
-for file in `find $DOTFILES_DIR -depth 1 -name ".*"`; do
+for file in `find $DOTFILES_DIR -depth 1 -name ".*" -type f` $DOTFILES_DIR/.zsh; do
   echo "ln -sf $file ~/"
   ln -sf $file ~/
 done
